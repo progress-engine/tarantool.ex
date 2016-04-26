ea6b220c647058c5b034048ee95a7680b3f2cfaf
Alexander Malaev <am@malaev.net>
2016-04-27 02:27:49 +0300

    Fix handling of incomplete responses

--- lib/tarantool.ex
+++ lib/tarantool.ex
@@ -1,24 +1,24 @@
 defmodule Tarantool do
-	@moduledoc """
-	Tarantool client for Elixir
-	"""
+  @moduledoc """
+  Tarantool client for Elixir
+  """
 
-	use Connection
-	use Tarantool.Constants
+  use Connection
+  use Tarantool.Constants
 
-	require Logger
+  require Logger
 
-	def start_link(host \\ 'localhost', port \\ 3301, timeout \\ 5000) do
+  def start_link(host \\ 'localhost', port \\ 3301, timeout \\ 5000) do
     Connection.start_link(__MODULE__, {host, port, timeout})
   end
 
-	def auth(conn, opts) do
-		Connection.call(conn, {:auth, opts})
-	end
+  def auth(conn, opts) do
+    Connection.call(conn, {:auth, opts})
+  end
 
-	def ping(conn, opts \\ %{}) do
-		Connection.call(conn, {:ping, opts})
-	end
+  def ping(conn, opts \\ %{}) do
+    Connection.call(conn, {:ping, opts})
+  end
 
   def select(conn, opts) do
     Connection.call(conn, {:select, opts})
@@ -53,19 +53,19 @@ defmodule Tarantool do
   end
 
   def close(conn) do
-		 Connection.call(conn, :close)
-	end
+     Connection.call(conn, :close)
+  end
 
   def init({host, port, timeout}) do
-    s = %{host: host, port: port, timeout: timeout, sock: nil, salt: nil, sync: 0, queue: %{}}
+    s = %{host: host, port: port, timeout: timeout, sock: nil, salt: nil, sync: 0, queue: %{}, response_size: nil, tail: ""}
     {:connect, :init, s}
   end
 
   def connect(_, %{sock: nil, host: host, port: port, timeout: timeout} = s) do
     case :gen_tcp.connect(host, port, [active: false, packet: :raw, mode: :binary], timeout) do
       {:ok, sock} ->
-				{:ok, _greeting, salt} = read_greeting(sock)
-				:ok = :inet.setopts(sock, active: :once)
+        {:ok, _greeting, salt} = read_greeting(sock)
+        :ok = :inet.setopts(sock, active: :once)
         {:ok, %{s | sock: sock, salt: salt}}
       {:error, _} ->
         {:backoff, 1000, s}
@@ -86,15 +86,15 @@ defmodule Tarantool do
     {:connect, :reconnect, %{s | sock: nil}}
   end
 
-	# Callbacks
+  # Callbacks
 
   def handle_call(_, _, %{sock: nil} = s) do
     {:reply, {:error, :closed}, s}
   end
 
-	def handle_call({code, opts}, from, %{sock: sock} = s) do
-		make_payload(code, opts, s)
-		|> send_request(sock)
+  def handle_call({code, opts}, from, %{sock: sock} = s) do
+    make_payload(code, opts, s)
+    |> send_request(sock)
 
     {:noreply, %{s | sync: s.sync + 1, queue: Map.put(s.queue, s.sync, from)}}
   end
@@ -103,40 +103,63 @@ defmodule Tarantool do
     {:disconnect, {:close, from}, s}
   end
 
-	@doc false
-	def handle_info({:tcp, _, packet_data}, %{sock: socket} = s) do
-		{:ok, {packet_length, data}} = MessagePack.unpack_once(packet_data)
-		{:ok, {header, body}} = MessagePack.unpack_once(data)
-		{:ok, {body, _rest}} = MessagePack.unpack_once(body)
+  @doc false
+  def handle_info({:tcp, _, data}, %{sock: socket, tail: tail} = s) do
+    s = parse_data(tail <> data, s)
 
-		sync = header[@iproto_keys[:sync]]
+    :inet.setopts(socket, active: :once)
+    {:noreply, s}
+  end
 
-		GenServer.reply(s.queue[sync], {:ok, header, body})
 
-		:inet.setopts(socket, active: :once)
-		{:noreply, %{s | queue: Map.delete(s.queue, sync)}}
-	end
+  def parse_data(data, %{response_size: nil} = s) do
+    case MessagePack.unpack_once(data) do
+      {:ok, {response_size, rest}} ->
+        parse_data(rest, %{s | response_size: response_size})
+      {:error, _} ->
+        %{s| tail: data}
+    end
+  end
 
-	defp make_payload(:auth, %{username: username, password: password}, %{salt: salt} = s) do
-		make_payload(:auth, %{username: username, tuple: ["chap-sha1", scramble(salt, password)]}, s)
-	end
+  def parse_data(data, %{response_size: response_size} = s ) do
+    cond do
+      byte_size(data) >= response_size ->
+        parse_response(data, s)
+      true ->
+        %{s| tail: data}
+    end
+  end
 
-	defp make_payload(code, opts, s) do
-		payload = pack_header(code, s) <> pack_body(opts)
-		payload_size(payload) <> payload
-	end
+  def parse_response(data, s) do
+    {:ok, {header, body}} = MessagePack.unpack_once(data)
+    {:ok, {body, rest}} = MessagePack.unpack_once(body)
 
-	defp pack_header(code, s) do
-		%{@iproto_keys[:request_type] => @iproto_codes[code],
+    sync = header[@iproto_keys[:sync]]
+    GenServer.reply(s.queue[sync], {:ok, header, body})
+
+    %{s| tail: rest, response_size: nil, queue: Map.delete(s.queue, sync)}
+  end
+
+  defp make_payload(:auth, %{username: username, password: password}, %{salt: salt} = s) do
+    make_payload(:auth, %{username: username, tuple: ["chap-sha1", scramble(salt, password)]}, s)
+  end
+
+  defp make_payload(code, opts, s) do
+    payload = pack_header(code, s) <> pack_body(opts)
+    payload_size(payload) <> payload
+  end
+
+  defp pack_header(code, s) do
+    %{@iproto_keys[:request_type] => @iproto_codes[code],
       @iproto_keys[:sync] => s.sync}
-		|> MessagePack.pack!
-	end
+    |> MessagePack.pack!
+  end
 
-	defp send_request(request, sock) do
-		:gen_tcp.send(sock, request)
-	end
+  defp send_request(request, sock) do
+    :gen_tcp.send(sock, request)
+  end
 
-	defp read_greeting(conn) do
+  defp read_greeting(conn) do
     {:ok, response } = :gen_tcp.recv(conn, 128)
 
     << greeting::512, salt::352, _rest :: binary >> = response
@@ -160,22 +183,22 @@ defmodule Tarantool do
     pack_body(rest, body, acc)
   end
 
-	defp payload_size(payload) do
-		<< 0xCE :: 8, byte_size(payload) :: 32-big-unsigned-integer-unit(1) >>
-	end
+  defp payload_size(payload) do
+    << 0xCE :: 8, byte_size(payload) :: 32-big-unsigned-integer-unit(1) >>
+  end
 
-	defp scramble(encoded_salt, password) do
-		<<salt :: 160, _ :: binary>> = :base64.decode(encoded_salt)
-		step1 = :crypto.hash(:sha, password)
-		step2 = :crypto.hash(:sha, step1)
-		step3 = :crypto.hash(:sha, [<<salt :: 160>>, step2])
-		:crypto.exor(step3, step1)
-	end
+  defp scramble(encoded_salt, password) do
+    <<salt :: 160, _ :: binary>> = :base64.decode(encoded_salt)
+    step1 = :crypto.hash(:sha, password)
+    step2 = :crypto.hash(:sha, step1)
+    step3 = :crypto.hash(:sha, [<<salt :: 160>>, step2])
+    :crypto.exor(step3, step1)
+  end
 
-	defp add_param(map, param_name, value) do
-		case value do
-			nil -> map
-			_ -> Map.merge(map, %{@iproto_keys[param_name] => value})
-		end
-	end
+  defp add_param(map, param_name, value) do
+    case value do
+      nil -> map
+      _ -> Map.merge(map, %{@iproto_keys[param_name] => value})
+    end
+  end
 end
