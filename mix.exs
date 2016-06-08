37112c21a134a9f835ff5bf49f6188574d39dbdd
Alexander Malaev <am@malaev.net>
2016-06-09 01:32:23 +0300

    Bump version to 0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tarantool.Mixfile do
 
   def project do
     [app: :tarantool,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
