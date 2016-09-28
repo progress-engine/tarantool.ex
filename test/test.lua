91cdb91ed8117f93a6a6572b97c5234cce0f1ca6
Alexander Malaev <am@malaev.net>
2016-09-29 01:23:30 +0300

    Add tests for function calls

--- test/test.lua
+++ test/test.lua
@@ -1,17 +1,31 @@
 box.cfg({listen=3303})
 box.schema.user.create('test', {password='t3st', if_not_exists=true})
 
+function test_func_1(p1)
+  return {'ok', p1}
+end
+
+function test_func_2(p1)
+  result = {
+    params = {
+      id = p1
+    }
+  }
+
+  return result
+end
+
 if not box.space.test then
-    local s1 = box.schema.space.create('test', {id = 513, if_not_exists = true})
-    local ip = s1:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
-    local iname = s1:create_index('name', {type = 'tree', parts = {2, 'STR'}, if_not_exists = true})
-    local irtree = s1:create_index('point', {type = 'rtree', unique=false, parts = {3, 'ARRAY'}, if_not_exists = true})
-    local ipname = s1:create_index('id_name', {type = 'tree', parts = {1, 'NUM', 2, 'STR'}})
+  local s1 = box.schema.space.create('test', {id = 513, if_not_exists = true})
+  local ip = s1:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
+  local iname = s1:create_index('name', {type = 'tree', parts = {2, 'STR'}, if_not_exists = true})
+  local irtree = s1:create_index('point', {type = 'rtree', unique=false, parts = {3, 'ARRAY'}, if_not_exists = true})
+  local ipname = s1:create_index('id_name', {type = 'tree', parts = {1, 'NUM', 2, 'STR'}})
 end
 
 if not box.space.test1 then
-    local s2 = box.schema.space.create('test1', {id = 514, if_not_exists = true})
-    local ip = s2:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
+  local s2 = box.schema.space.create('test1', {id = 514, if_not_exists = true})
+  local ip = s2:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
 end
 
 local s1 = box.space.test
@@ -24,5 +38,5 @@ s2:insert{1, "hello", {1, 2}, 100}
 s2:insert{2, "world", {3, 4}, 200}
 
 pcall(function()
-box.schema.user.grant('guest', 'read,write,execute', 'universe')
+  box.schema.user.grant('guest', 'read,write,execute', 'universe')
 end)
